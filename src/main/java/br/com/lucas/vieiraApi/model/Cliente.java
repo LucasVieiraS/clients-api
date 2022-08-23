38d02773bf37b18884bab1d083fcddd5b429d4bb
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-08-23 09:33:39 -0300

    new: tables

---
+++ src/main/java/br/com/lucas/vieiraApi/model/Cliente.java
@@ -0,0 +1,44 @@
+package br.com.lucas.vieiraApi.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id) && Objects.equals(nome, cliente.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+}
---
+++ src/main/java/br/com/lucas/vieiraApi/model/Contato.java
@@ -0,0 +1,67 @@
+package br.com.lucas.vieiraApi.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="contato")
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "idcliente")
+    private Long idcliente;
+
+    private String descricao;
+    private String contato;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(Long idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato1 = (Contato) o;
+        return Objects.equals(id, contato1.id) && Objects.equals(idcliente, contato1.idcliente) && Objects.equals(descricao, contato1.descricao) && Objects.equals(contato, contato1.contato);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, idcliente, descricao, contato);
+    }
+}
