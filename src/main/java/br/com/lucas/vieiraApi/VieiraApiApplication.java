d33b3ccbfeaf41673f074f7dd81b0b9734ea8b2a
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-08-23 09:06:33 -0300

    Initial commit

---
+++ src/main/java/br/com/lucas/vieiraApi/VieiraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.vieiraApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VieiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VieiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucas/vieiraApi/VieiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.vieiraApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VieiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
