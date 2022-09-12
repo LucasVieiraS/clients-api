74ce3cdaa3f7be37f246711cf850e9685ddd975a
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-09-12 08:13:50 -0300

    refactor: application properties

--- src/main/java/br/com/lucas/vieiraApi/VieiraApiApplication.java
+++ src/main/java/br/com/lucas/vieiraApi/VieiraApiApplication.java
@@ -2,13 +2,14 @@ package br.com.lucas.vieiraApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class VieiraApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VieiraApiApplication.class, args);
-		System.out.println("Successfully initialized API.");
 	}
 
 }
--- src/main/java/br/com/lucas/vieiraApi/model/Contato.java
+++ src/main/java/br/com/lucas/vieiraApi/model/Contato.java
@@ -1,7 +1,5 @@
 package br.com.lucas.vieiraApi.model;
 
-import com.sun.istack.NotNull;
-
 import javax.persistence.*;
 import java.util.Objects;
 
