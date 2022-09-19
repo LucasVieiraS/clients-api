0e9071c905cefe76bc675b20ef9f51828806d6b5
Lucas <lucasvieirasilva.lpta@gmail.com>
2022-09-19 08:55:45 -0300

    new: added fk contato->cliente + inserts
    FK_Contato_Cliente

--- src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
+++ src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
@@ -3,3 +3,8 @@ CREATE TABLE IF NOT EXISTS Cliente (
     nome VARCHAR(255) NOT NULL,
     PRIMARY KEY(id)
 );
+
+INSERT INTO Cliente(nome)
+VALUES  ("Pedro"),
+        ("Carlão"),
+        ("João");
--- src/main/resources/db/migration/V002__Criar_Tabela_Contato.sql
+++ src/main/resources/db/migration/V002__Criar_Tabela_Contato.sql
@@ -5,3 +5,13 @@ CREATE TABLE IF NOT EXISTS Contato (
     contato VARCHAR(100) NOT NULL,
     PRIMARY KEY(ID)
 );
+
+ALTER TABLE Contato
+ADD CONSTRAINT FK_Contato_Cliente
+FOREIGN KEY(cliente)
+REFERENCES Cliente(id);
+
+INSERT INTO Contato(cliente, descricao, contato)
+VALUES  (1, "Encanador", "+(55) 14 99610-2925"),
+        (2, "Jogador de Basquete", "+(55) 14 99723-3221"),
+        (3, "Jogador de Baseball", "+(55) 14 99326-1728");
